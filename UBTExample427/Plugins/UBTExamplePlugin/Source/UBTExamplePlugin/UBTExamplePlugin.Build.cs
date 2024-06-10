6a55b5aa3478cd27126e9d0a6294f54c5fb649ab
Joel Burke <jburke@kittehface.com>
2024-06-10 18:51:26 -0400

    Access command line arguments from a plugin.

--- UBTExample427/Plugins/UBTExamplePlugin/Source/UBTExamplePlugin/UBTExamplePlugin.Build.cs
+++ UBTExample427/Plugins/UBTExamplePlugin/Source/UBTExamplePlugin/UBTExamplePlugin.Build.cs
@@ -2,14 +2,25 @@
 
 using UnrealBuildTool;
 
+// Required for CommandLine attribute and CommandLineArguments
+using Tools.DotNETCommon;
+
 public class UBTExamplePlugin : ModuleRules
 {
+	[CommandLine("-pluginsetting")]
+	public bool bPluginSetting = false;
+
 	public UBTExamplePlugin(ReadOnlyTargetRules Target) : base(Target)
 	{
+		CommandLineArguments commandLineArguments = new CommandLineArguments(System.Environment.GetCommandLineArgs());
+		commandLineArguments.ApplyTo(this);
+
 		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
 		
 		PublicDependencyModuleNames.AddRange(new string[] { "Core" });
 		
 		PrivateDependencyModuleNames.AddRange(new string[] { "CoreUObject", "Engine", "Slate", "SlateCore" });
+
+		Log.TraceInformation(string.Format("UBTExamplePlugin: plugin setting value is set - {0}", bPluginSetting));
 	}
 }
--- UBTExample54/Plugins/UBTExamplePlugin/Source/UBTExamplePlugin/UBTExamplePlugin.Build.cs
+++ UBTExample54/Plugins/UBTExamplePlugin/Source/UBTExamplePlugin/UBTExamplePlugin.Build.cs
@@ -2,14 +2,28 @@
 
 using UnrealBuildTool;
 
+// Required for CommandLine attribute and CommandLineArguments
+using EpicGames.Core;
+
+// Required for logging
+using Microsoft.Extensions.Logging;
+
 public class UBTExamplePlugin : ModuleRules
 {
+	[CommandLine("-pluginsetting")]
+	public bool bPluginSetting = false;
+
 	public UBTExamplePlugin(ReadOnlyTargetRules Target) : base(Target)
 	{
+		CommandLineArguments commandLineArguments = new CommandLineArguments(System.Environment.GetCommandLineArgs());
+		commandLineArguments.ApplyTo(this);
+
 		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
 		
 		PublicDependencyModuleNames.AddRange(new string[] { "Core" });
 		
 		PrivateDependencyModuleNames.AddRange(new string[] { "CoreUObject", "Engine", "Slate", "SlateCore" });
+
+		Logger.LogInformation(string.Format("UBTExamplePlugin: plugin setting value is set - {0}", bPluginSetting));
 	}
 }
