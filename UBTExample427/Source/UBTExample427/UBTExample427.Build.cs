517578d7422d4d7914b13a3463469ecc9564bbd9
Joel Burke <jburke@kittehface.com>
2024-06-10 18:46:25 -0400

    Accessing command line arguments from a build module.

--- UBTExample427/Source/UBTExample427/UBTExample427.Build.cs
+++ UBTExample427/Source/UBTExample427/UBTExample427.Build.cs
@@ -2,12 +2,23 @@
 
 using UnrealBuildTool;
 
+// Required for CommandLine attribute and CommandLineArguments
+using Tools.DotNETCommon;
+
 public class UBTExample427 : ModuleRules
 {
+	[CommandLine("-modulesetting")]
+	public bool bModuleSetting = false;
+
 	public UBTExample427(ReadOnlyTargetRules Target) : base(Target)
 	{
+		CommandLineArguments commandLineArguments = new CommandLineArguments(System.Environment.GetCommandLineArgs());
+		commandLineArguments.ApplyTo(this);
+
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+
+		Log.TraceInformation(string.Format("UBTExample: module setting value is set - {0}", bModuleSetting));
 	}
 }
--- UBTExample54/Source/UBTExample54/UBTExample54.Build.cs
+++ UBTExample54/Source/UBTExample54/UBTExample54.Build.cs
@@ -2,14 +2,28 @@
 
 using UnrealBuildTool;
 
+// Required for CommandLine attribute and CommandLineArguments
+using EpicGames.Core;
+
+// Required for logging
+using Microsoft.Extensions.Logging;
+
 public class UBTExample54 : ModuleRules
 {
+	[CommandLine("-modulesetting")]
+	public bool bModuleSetting = false;
+
 	public UBTExample54(ReadOnlyTargetRules Target) : base(Target)
 	{
+		CommandLineArguments commandLineArguments = new CommandLineArguments(System.Environment.GetCommandLineArgs());
+		commandLineArguments.ApplyTo(this);
+
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
+
+		Logger.LogInformation(string.Format("UBTExample: module setting value is set - {0}", bModuleSetting));
 	}
 }
