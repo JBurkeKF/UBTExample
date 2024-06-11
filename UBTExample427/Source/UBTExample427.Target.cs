cf163fc8ab2913e42d282ca7335e71ec06e0bb5a
Joel Burke <jburke@kittehface.com>
2024-06-11 18:41:28 -0400

    Added some more commentary about accessing shared code from Target objects.

--- UBTExample427/Source/UBTExample427.Target.cs
+++ UBTExample427/Source/UBTExample427.Target.cs
@@ -19,9 +19,18 @@ public class UBTExample427Target : TargetRules
 
 		Log.TraceInformation(string.Format("UBTExample: target setting value is set - {0}", bTargetSetting));
 
+		// The target is created multiple times at different stages of the Unreal build process, and
+		// depending on the stage, it may or may not have full context to access the contents of
+		// the various modules.  It will not compile correctly with direct references to code in
+		// other modules, so if we really want to access that code, we must use reflection.
 		System.Reflection.Assembly buildAssembly = System.Reflection.Assembly.GetExecutingAssembly();
 		System.Type ubtExamplePluginExampleModuleType = null;
 
+		// Under most circumstances, trying to get the module type will just result in a null value
+		// that we can check against.  However, if this is running in an Unreal Engine that is in
+		// source code format (instead of a built engine like those provided in the Epic Games
+		// Launcher), the .GetType() call will actually throw an exception trying to access
+		// the type.  We must wrap this in a try/catch so the Unreal build process can proceed.
 		try
 		{
 			ubtExamplePluginExampleModuleType = buildAssembly.GetType("UBTExamplePluginExampleModule");
